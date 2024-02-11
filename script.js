774db8a03a9434bf6d041874a9f1d83ac8c593d2
DMYTRO TSYHANENKO <DMYTRO.TSYHANENKO@OUTLOOK.COM>
2024-02-11 20:32:34 +0100

    hardcoded city

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const form = document.getElementById("form")
-const WEATHER_API_CALL =
-  "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=28a377ca6ef3c6e32b3f3f51588ebb85"
 
 form.addEventListener("keypress", (e) => {
   if (e.key != "Enter" || e.target.value === "") {
@@ -8,12 +6,13 @@ form.addEventListener("keypress", (e) => {
   } else {
     e.preventDefault()
     e.target.value = ""
+    let API_CALL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=28a377ca6ef3c6e32b3f3f51588ebb85`
     getWeather()
+
+    async function getWeather() {
+      const res = await fetch(API_CALL)
+      const weather = await res.json()
+      console.log(weather)
+    }
   }
 })
-
-async function getWeather() {
-  const res = await fetch(WEATHER_API_CALL)
-  const weather = await res.json()
-  console.log(weather)
-}
