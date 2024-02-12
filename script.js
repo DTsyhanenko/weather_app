b5e860150c9a6b832751541409eac0f6ae0af2df
DMYTRO TSYHANENKO <DMYTRO.TSYHANENKO@OUTLOOK.COM>
2024-02-12 21:50:32 +0100

    current weather info

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const container = document.querySelector(".form-container")
 const form = document.getElementById("form")
 
 form.addEventListener("keypress", (e) => {
@@ -13,8 +14,32 @@ form.addEventListener("keypress", (e) => {
 
     async function getWeather() {
       const res = await fetch(API_CALL)
-      const weather = await res.json()
-      console.log(weather)
+      const data = await res.json()
+      const cityName = data.name
+      const weatherDescrition = data.weather[0].main
+      const tempCelcius = Math.round(data.main.temp - 273)
+      console.log(data)
+
+      const weatherInfos = `
+      <p id="selected">Selected city: ${cityName}</p>
+      <div class="show weather-info">
+          <div id="current">
+              <div class="weather-data temp">
+                  <p class="celsius">${tempCelcius}</p>
+                  <p>${weatherDescrition}</p>
+              </div>
+              <div class="weather-data city">
+                  <p>Clouds</p>
+                  <p>${cityName}, ${data.sys.country}</p>
+              </div>
+              <div class="weather-data icons">
+                  <img class="weather-icon" src="./src/rain.png" alt="Cloud with rain">
+              </div>
+          </div>
+          <div id="week"></div>
+      </div>
+      `
+      container.insertAdjacentHTML("beforeend", weatherInfos)
     }
   }
 })
