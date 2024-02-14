f9802b2699ab4f86d64af21d4c53ebfe66fbbc73
DMYTRO TSYHANENKO <DMYTRO.TSYHANENKO@OUTLOOK.COM>
2024-02-14 21:11:14 +0100

    resolved some issues

--- script.js
+++ script.js
@@ -2,44 +2,62 @@ const container = document.querySelector(".form-container")
 const form = document.getElementById("form")
 
 form.addEventListener("keypress", (e) => {
-  if (e.key != "Enter" || e.target.value === "") {
-    return
-  } else {
+  if (e.key === "Enter" && e.target.value !== "") {
     e.preventDefault()
-    const API_CALL = `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=28a377ca6ef3c6e32b3f3f51588ebb85`
+
+    const API_CALL = `https://api.openweathermap.org/data/2.5/forecast?q=${e.target.value}&appid=28a377ca6ef3c6e32b3f3f51588ebb85&units=metric`
     getWeather()
     e.target.value = ""
 
     async function getWeather() {
-      const res = await fetch(API_CALL)
-      const data = await res.json()
-      const cityName = data.name
-      const weatherDescrition = data.weather[0].main
-      const tempCelcius = Math.round(data.main.temp - 273)
-      const clouds = data.clouds.all
+      try {
+        const res = await fetch(API_CALL)
+        const data = await res.json()
+        const dataList = data.list
+        console.log(dataList)
 
-      console.log(data)
+        const cityName = data.city.name
+        const weatherDescrition = data.list[0].weather[0].main
+        const tempCelcius = Math.round(data.list[0].main.temp)
+        const clouds = data.list[0].clouds.all
 
-      const weatherInfos = `
-      <p id="selected">Selected city: ${cityName}</p>
-      <div class="weather-info">
-          <div id="current">
-              <div class="weather-data temp">
-                  <p class="celsius">${tempCelcius} °C</p>
+        const weatherInfos = `
+            <p id="selected">Selected city: ${cityName}</p>
+            <div class="weather-info">
+              <div id="current">
+                <div class="weather-data temp">
+                  <p class="celsius">${tempCelcius}°C</p>
                   <p>${weatherDescrition}</p>
+                </div>
+                <div class="weather-data city">
+                  <p>Clouds: ${clouds} %</p>
+                  <p>${cityName}</p>
+                </div>
+                <div class="weather-data icons">
+                  <img class="weather-icon" src="./assets/rain.png" alt="Cloud with rain">
+                </div>
               </div>
-              <div class="weather-data city">
-                  <p>Clouds ${clouds}%</p>
-                  <p>${cityName}, ${data.sys.country}</p>
+              <div class="week">
+                <div class="week-day">THU</div>
+                <img class="weather-icon" src="./assets/sun-clouds.png" alt="">
+                <p class="weather-description">Sunny, Cloudly</p>
+                <div class="day-night-temp">
+                <div class="day">
+                  <p class="day-time">Day</p>
+                  <p class="daily-temp">4°C</p>
+                </div>
+                <div class="night">
+                  <p class="daily-temp">1°C</p>
+                  <p class="day-time">Night</p>
+                </div>
               </div>
-              <div class="weather-data icons">
-                  <img class="weather-icon" src="./src/rain.png" alt="Cloud with rain">
-              </div>
-          </div>
-          <div id="week"></div>
-      </div>
-      `
-      container.insertAdjacentHTML("beforeend", weatherInfos)
+            </div>
+          `
+        container.insertAdjacentHTML("beforeend", weatherInfos)
+      } catch (err) {
+        console.log(err)
+        /* alert("An error ocurred!") */
+      }
     }
   }
 })
